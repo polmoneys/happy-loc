f1007b5654e4d27581128d29a1bf37111f913e5a
Pol Moneys <pmones@wideagency.com>
2022-03-10 18:28:07 +0100

    feat: adds Tooltip component

---
+++ src/components/Tooltip/Tooltip.tsx
@@ -0,0 +1,137 @@
+/**
+ *
+ * 2021. Pol Moneys
+ * Tooltip 1.0.0
+ * Feedback at polmoneys on github
+ *
+ */
+
+import { Popover } from "@headlessui/react";
+import { useFocusRing } from "@react-aria/focus";
+import { useHover } from "@react-aria/interactions";
+import { Fragment, ReactNode, useEffect, useRef } from "react";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+
+import Button from "@/components/Button/Button";
+import { HelveticaNeue } from "@/components/Font/Font";
+
+import useStyles from "../../hooks/UseStyles/UseStyles";
+import { CSSProps, DefaultProps } from "../types";
+import styles from "./Tooltip.module.css";
+
+const { Group } = Popover;
+
+interface Props extends Pick<DefaultProps, "className" | "children"> {
+  label: string | ReactNode;
+  closeLabel?: string | ReactNode;
+  variant?: "inline" | "input";
+  onHoverIn?: () => void;
+  onHoverOut?: () => void;
+  offset?: {
+    start?: string;
+    x?: string;
+    y?: string;
+  };
+}
+
+const Tooltip = (props: Props) => {
+  const {
+    children,
+    label,
+    onHoverIn,
+    onHoverOut,
+    variant = "input",
+    offset,
+    className,
+    closeLabel,
+  } = props;
+
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const { isFocusVisible, focusProps } = useFocusRing();
+
+  const { hoverProps, isHovered } = useHover({
+    onHoverStart: event => onHoverIn?.(),
+    onHoverEnd: event => onHoverOut?.(),
+  });
+
+  useEffect(() => {
+    if (isHovered && buttonRef?.current !== null) {
+      buttonRef?.current.click();
+    }
+  }, [isHovered]);
+
+  let propsToStyles: CSSProps = {};
+
+  if (offset !== undefined) {
+    propsToStyles = {
+      ...(offset.start !== undefined && {
+        "--tooltip-offset-start": offset.start,
+      }),
+      ...(offset.x !== undefined && { "--tooltip-offset-x": offset.x }),
+      ...(offset.y !== undefined && { "--tooltip-offset-y": offset.y }),
+    };
+  }
+
+  const { output: triggerClassNames } = useStyles(
+    styles.button,
+    variant === "input" ? styles.input : styles.inline
+  );
+
+  const { output: rootClassNames } = useStyles(className, styles.root);
+  const idleChevronColor = isFocusVisible
+    ? "var(--teal-5)"
+    : variant === "input"
+    ? "currentColor"
+    : "var(--gray-4)";
+  return (
+    <Popover className={rootClassNames}>
+      {({ open }) => (
+        <Fragment>
+          <Popover.Button
+            {...hoverProps}
+            {...focusProps}
+            ref={buttonRef}
+            className={triggerClassNames}
+          >
+            <HelveticaNeue as="span">{label}</HelveticaNeue>
+            {open ? (
+              <FiChevronUp />
+            ) : (
+              <FiChevronDown color={idleChevronColor} />
+            )}
+          </Popover.Button>
+          {/* <Popover.Overlay className="overlay" /> */}
+
+          <Popover.Panel
+            className={styles.content}
+            style={{
+              ...propsToStyles,
+            }}
+          >
+            {({ close }) => (
+              <Fragment>
+                {children}
+                {closeLabel !== undefined ? (
+                  <Button
+                    color="text"
+                    className={styles.close}
+                    onClick={() => {
+                      close();
+                    }}
+                  >
+                    {closeLabel}
+                  </Button>
+                ) : (
+                  <Fragment />
+                )}
+              </Fragment>
+            )}
+          </Popover.Panel>
+        </Fragment>
+      )}
+    </Popover>
+  );
+};
+
+export default Tooltip;
+export { Group as TooltipGroup };
